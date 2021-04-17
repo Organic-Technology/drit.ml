c2f06e1b1b83ad6df99aac1a441ebceea3328960
Clement N. Inga <cinga@gradual.gq>
2021-04-17 07:55:48 +0200

    added a black and white bg

--- app.js
+++ app.js
@@ -2,6 +2,6 @@
 //  background-image: url(bg-kidazuf.webp);
 
 var body = document.getElementsByTagName('body')[0];
-body.style.backgroundImage = 'url(bg-kidazuf.webp)';
+body.style.backgroundImage = 'url(bg-drit_opt.webp)';
 
 
