7a92f0b5dd74eb6f4d259ba05f1e4ccbacc7390d
Doug F. Sosso <dsosso@gatetm.tk>
2021-04-17 12:56:17 +0200

    added a svg background

--- app.js
+++ app.js
@@ -2,6 +2,6 @@
 //  background-image: url(bg-kidazuf.webp);
 
 var body = document.getElementsByTagName('body')[0];
-//body.style.backgroundImage = 'url(bg-drit_opt.webp)';
+//body.style.backgroundImage = 'url(background.svg)';
 
 
