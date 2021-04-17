a0f3c602cbe09776c2ec5c789de95d529590eb6c
Megan K. Neidiger <mneidiger@keysm.ml>
2021-04-17 14:17:04 +0200

    added a google favicon comparison !

--- build.sh
+++ build.sh
@@ -7,6 +7,10 @@ gzip -f -9 --keep *.js
 gzip -f -9 --keep *.svg
 gzip -f -9 --keep *.png
 
+convert favicon.svg -resize 16x16 -strip favicon.png
+pngquant -f --quality 0-50 --speed 1 --strip --verbose favicon.png
+convert favicon.svg -resize 16x16 -strip favicon.webp
+
 rm sizes*.webp
 pngquant -f --quality 0-50 --speed 1 --strip --verbose sizes.png
 convert sizes.png -quality 50 -strip sizes.png sizes.webp
